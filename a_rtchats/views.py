60c37907fc2b6b41d3df69380f0173f59b54d231
“Dankyi <dankyibenjamin0@gmail.com>
2024-12-19 10:14:45 +0000

    websocket connection established

--- a_rtchats/views.py
+++ a_rtchats/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def chat_view(request):
-    chat_group = get_object_or_404(ChatGroup, group_name="Public Messages")
+    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
     chat_messages = chat_group.chat_messages.all()[:30]
     form = ChatmessageCreateForm()
 
