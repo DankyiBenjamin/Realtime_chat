b653a812e3e0080be52ca08f58cab63ef671f221
“Dankyi <dankyibenjamin0@gmail.com>
2024-12-20 12:37:17 +0000

    Add online user tracking and environment-based database configuration

--- a_core/settings.py
+++ a_core/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,21 +102,43 @@ TEMPLATES = [
 ASGI_APPLICATION = 'a_core.asgi.application'
 
 
-CHANNEL_LAYERS = {
-    'default': {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    }
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     }
+# }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(os.getenv('REDIS_URL'))],
+        },
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+# deployement database configuaration
+if os.getenv('DEPLOYMENT') == 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
 
 
 # Password validation
--- a_rtchats/consumers.py
+++ a_rtchats/consumers.py
@@ -16,12 +16,20 @@ class ChatroomConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_add)(
             self.chatroom_name, self.channel_name
         )
+        # add and update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name, self.channel_name
         )
+        # remove user from online users
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -54,4 +62,26 @@ class ChatroomConsumer(WebsocketConsumer):
 
         html = render_to_string(
             "a_rtchats/partials/chat_message_p.html", context=context)
+        self.send(text_data=html)
+
+    def update_online_count(self):
+        # get the online users count and subtract the current user
+        online_count = self.chatroom.users_online.count() - 1
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count,
+        }
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+        context = {
+            'online_count': online_count,
+        }
+
+        html = render_to_string(
+            "a_rtchats/partials/online_count_p.html", context=context)
         self.send(text_data=html)
